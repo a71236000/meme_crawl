d1cf2eaf6d1324842993240073d2db769f186c4b
a71236000 <a71236000@gmail.com>
2023-02-08 09:46:50 +0800

    init

--- main.py
+++ main.py
@@ -16,19 +16,35 @@ def crawl_from_ig():
     wd = webdriver.Chrome(service=Service('C:\chromedriver.exe'))
     wd.implicitly_wait(8)
     wd.maximize_window()
-    wd.get("https://www.instagram.com/godreplyme/")
-    wd.find_element(By.XPATH,"//*[text()='登入']").click()
+
+    time.sleep(1)
+
+    wd.get("https://www.instagram.com")
+
+    time.sleep(1)
+
+
 
     wd.find_element(By.NAME, "username").send_keys("testcrawl1234")
-    wd.find_element(By.NAME,"password").send_keys("")
+    wd.find_element(By.NAME,"password").send_keys("bb617091")
 
     wd.find_element(By.XPATH,"//*[text()='登入']").click()
     wd.find_element(By.XPATH,"//*[text()='稍後再說']").click()
-
+    wd.find_element(By.XPATH, "//*[text()='稍後再說']").click()
     time.sleep(1)
-    # 定位到 Class name "_aagw" 下面的所有圖片
-    images = wd.find_elements(By.CSS_SELECTOR,"._aagw img")
+    wd.get("https://www.instagram.com/godreplyme")
+    scroll_time = int(input('請輸入想要捲動幾次'))
 
+    for now_time in range(1, scroll_time + 1):
+        time.sleep(1)
+        print(f"now scroll {now_time}/{scroll_time}")
+        js = "window.scrollTo(0, document.body.scrollHeight);"
+        wd.execute_script(js)
+    print('scroll_finish')
+    time.sleep(2)
+    # 定位到 Class name "_aagw" 下面的所有圖片
+    images = wd.find_elements(By.XPATH,"//div[@class='_aagv']/img")
+    print(images)
     # 遍歷所有圖片，下載並保存為 JPG 檔
     for i, image in enumerate(images):
         src = image.get_attribute("src")
@@ -36,19 +52,6 @@ def crawl_from_ig():
         with open(f"{i}.jpg", "wb") as f:
             f.write(response.content)
 
-    input()
-''' pic_list=wd.find_elements(By.CLASS_NAME,"_aagw")
-
-    wd.save_screenshot("screenshot.png")
 
 
-    for pic in pic_list:
-
-
-        time.sleep(1)
-        pic.click()
-        wd.save_screenshot("screenshot.png")
-        wd.find_element(By.TAG_NAME,"body").send_keys(Keys.ESCAPE)
-    input()
-'''
 crawl_from_ig()
